c48d961a735d9ec12d9775370b0dc6b9f05b6f7c
D1lanyGIT <136501413+D1lanyGIT@users.noreply.github.com>
2024-03-06 16:52:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching Rovision Song Contest..."];
 
 
 let currentIndex = 0;
